fa42b5dbf804062d543b32ec1f009db3960e4a82
maryandjohndeath <71418162+maryandjohndeath@users.noreply.github.com>
2020-09-16 22:27:02 -0700

    Update getpw.js

--- getpw.js
+++ getpw.js
@@ -1,7 +1,12 @@
+$(document).ready( function(){
 var ps = $('.password').val();
 
-$.get( "pw.json", function( p ) {
-  $( ".result" ).html( result );
-  if ( ps = result ) {
-  alert("right");
-});
+
+  $.get( "pw.json", function( p ) {
+    $( ".result" ).html( result );
+    if ( ps = result ) {
+    alert("right");
+  });
+    
+})
+  
